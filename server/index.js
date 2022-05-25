ad211d779321d7efee344b8473b9a478cfde8410
张志阵 <1809642355@qq.com>
2022-05-25 19:55:38 +0800

    feat:添加server文件验证内容

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
+/**
+ * 验证文件的强缓存
+ */
+
+// eslint-disable-next-line import/no-extraneous-dependencies
 const express = require('express');
 
 const app = express();
 
-app.use(express.static('../dist', { maxAge: 1000 * 3600 }))
+app.use(express.static('../dist', { maxAge: 1000 * 3600 }));
 
-app.listen(3500)
+app.listen(3500);
